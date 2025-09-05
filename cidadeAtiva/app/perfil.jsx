a4e7c713937f25f94f07f94c078f1bf6cbaa0330
vlademir fernando breve <vlademir.breve@gmail.com>
2025-09-05 09:45:37 -0300

    scroll nao deixa o teclado sobre os campos

--- cidadeAtiva/app/perfil.jsx
+++ cidadeAtiva/app/perfil.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Pressable, Alert } from "react-native";
+import { KeyboardAvoidingView, Platform, ScrollView, View, Text, TextInput, StyleSheet, Pressable, Alert } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { useRouter } from "expo-router";
 
@@ -52,42 +52,43 @@ export default function Perfil() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>DADOS PESSOAIS</Text>
-      <Text style={styles.subtitle}>Insira seus dados pessoais</Text>
+    <KeyboardAvoidingView
+      style={{ flex: 2 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.subtitle}></Text>
+        <Text style={styles.subtitle}></Text>
+        <Text style={styles.title}>DADOS PESSOAIS</Text>
+        <Text style={styles.subtitle}>Insira seus dados pessoais</Text>
 
-      <TextInput style={styles.input} placeholder="Nome" value={nome} onChangeText={setNome} />
-      <TextInput style={styles.input} placeholder="Sobrenome" value={sobrenome} onChangeText={setSobrenome} />
-      <TextInput style={styles.input} placeholder="CPF" value={cpf} onChangeText={setCpf} keyboardType="numeric" />
+        <TextInput style={styles.input} placeholder="Nome" value={nome} onChangeText={setNome} />
+        <TextInput style={styles.input} placeholder="Sobrenome" value={sobrenome} onChangeText={setSobrenome} />
+        <TextInput style={styles.input} placeholder="CPF" value={cpf} onChangeText={setCpf} keyboardType="numeric" />
 
-      <Picker selectedValue={tipoTelefone} style={styles.input} onValueChange={(itemValue) => setTipoTelefone(itemValue)}>
-        <Picker.Item label="Celular" value="Celular" />
-        <Picker.Item label="Residencial" value="Residencial" />
-        <Picker.Item label="Comercial" value="Comercial" />
-      </Picker>
+        <Picker selectedValue={tipoTelefone} style={styles.option} onValueChange={(itemValue) => setTipoTelefone(itemValue)}>
+          <Picker.Item label="Celular" value="Celular" />
+          <Picker.Item label="Residencial" value="Residencial" />
+          <Picker.Item label="Comercial" value="Comercial" />
+        </Picker>
 
-      <TextInput style={styles.input} placeholder="Telefone" value={telefone} onChangeText={setTelefone} keyboardType="phone-pad" />
-      <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} keyboardType="email-address" />
+        <TextInput style={styles.input} placeholder="Telefone" value={telefone} onChangeText={setTelefone} keyboardType="phone-pad" />
+        <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} keyboardType="email-address" />
 
-      <Pressable style={styles.button} onPress={salvarDados}>
-        <Text style={styles.buttonText}>Salvar</Text>
-      </Pressable>
-    </View>
+        <Pressable style={styles.button} onPress={salvarDados}>
+          <Text style={styles.buttonText}>Salvar</Text>
+        </Pressable>
+      </ScrollView >
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#f2f6fa", padding: 30, justifyContent: 'center', alignItens: 'center' },
+  container: { flexGrow: 1, height: 1, backgroundColor: "#191a27ff", padding: 30, justifyContent: 'center', alignItens: 'center' },
   title: { fontSize: 22, fontWeight: "bold", color: "#144480", marginBottom: 5 },
   subtitle: { fontSize: 14, color: "#666", marginBottom: 20 },
-  input: {
-    backgroundColor: "#fff",
-    padding: 12,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    marginBottom: 15,
-  },
+  input: { backgroundColor: "#fff", padding: 10, borderRadius: 8, borderWidth: 1, borderColor: "#ccc", marginBottom: 15, },
+  option: { backgroundColor: "#fff", height: 50, padding: 10, borderRadius: 8, borderWidth: 1, borderColor: "#ccc", marginBottom: 15, },
   button: { backgroundColor: "#144480", padding: 15, borderRadius: 10, alignItems: "center", marginTop: 20 },
   buttonText: { color: "#fff", fontWeight: "600", fontSize: 16 },
 });
